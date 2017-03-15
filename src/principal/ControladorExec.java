bc92bf4022d39c02c8579d699e7e18103c1a230b
Rubens <rubens_olv@hotmail.com>
2017-03-15 18:51:29 -0300

    Mudanças

--- src/principal/ControladorExec.java
+++ src/principal/ControladorExec.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashSet;
 
